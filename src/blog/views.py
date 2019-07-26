9d3c85b5cbfa4e1c2fa3b6c1454a108a3e8a8577
unknown <abdelrhman_m@outlook.com>
2019-07-26 09:03:58 +0200

    urls and make base html

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
 
+posts = [
+	{
+		'title' : 'first blog',
+		'content' : 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+		'date':'15-3-2019',
+		'author' : 'ahmed'
+
+	},
+	{
+		'title' : 'second blog',
+		'content' : '21 ipsum lorem ipsum lorem ipsum lorem ipsum ',
+		'date':'19-3-2019',
+		'author' : 'ali'
+
+	},
+	{
+		'title' : 'thirt blog',
+		'content' : '19 lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+		'date':'18-3-2019',
+		'author' : 'mohamed'
+
+	},
+	{
+		'title' : 'fourth blog',
+		'content' : 'iiilorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+		'date':'16-3-2019',
+		'author' : 'omar'
+
+	},
+]
 def home(request):
-	return render(request,'blog/index.html',{'title': 'home'})
+
+	context = {
+		'title' : 'الصفحة الرئيسية',
+		'posts' : posts
+	}
+	return render(request,'blog/index.html',context)
+
+def about(request):
+	return render(request,'blog/about.html',{'title' : 'من انا'})
